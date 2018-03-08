b3890c5c155b044f2012dbdb927f5bdd56bee5bd
gokulgopal <35801031+gokulgopal@users.noreply.github.com>
2018-03-08 10:39:07 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+void main()
+{
+int n,temp,i,j,b[20];
+printf("enter the array of numbers");
+scanf("%d",&n);
+printf("enter the numbers");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&b[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(b[i]>b[j])
+{
+temp=b[i];
+b[i]=b[j];
+b[j]=temp;
+}
+}
+}
+printf("%d",b[0]);
+}
